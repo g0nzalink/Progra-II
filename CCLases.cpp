fa485dad642cd08ff1ed8b7752c590cd36c22f0f
araooz <fali.araoz@utec.edu.pe>
2024-11-22 20:27:01 -0500

    archivos de clases

---
+++ CCLases.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ferdi on 22/11/24.
+//
+
+#include "CCLases.h"
---
+++ CCLases.h
@@ -0,0 +1,16 @@
+//
+// Created by ferdi on 22/11/24.
+//
+
+#ifndef CCLASES_H
+#define CCLASES_H
+
+
+
+class CCLases {
+
+};
+
+
+
+#endif //CCLASES_H
